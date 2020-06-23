c1b9e1d9cfa0d47b49fb896a193a9f29da87e164
Lotfi Anwar L Arif <52082662+DovaLuffy@users.noreply.github.com>
2020-06-24 00:05:50 +0800

    This is my newest commit

---
+++ Book adding/app6.js
@@ -0,0 +1,145 @@
+class Book {
+    constructor(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+
+class UI {
+
+    addBookToList(book) {
+        const list = document.getElementById('book-list');
+        // create tr element
+        const row = document.createElement('tr');
+
+        row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class ="delete">X<a></td>
+        `;
+
+        list.appendChild(row);
+    }
+    showAlert(message, className) {
+        const div = document.createElement('div');
+
+        div.className = `alert ${className}`;
+
+        div.appendChild(document.createTextNode(message));
+
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+
+        container.insertBefore(div, form);
+
+        setTimeout(function() {
+            document.querySelector('.alert').remove();
+        }, 3000)
+    }
+    deleteBook(target) {
+        if (target.className === 'delete') {
+            target.parentElement.parentElement.remove();
+        }
+    }
+    clearFields() {
+        document.getElementById('title').value = '';
+        document.getElementById('author').value = '';
+        document.getElementById('isbn').value = '';
+    }
+}
+class Store {
+    static getBooks() {
+        let books;
+        if (localStorage.getItem('books') === null) {
+            books = [];
+        } else {
+            books = JSON.parse(localStorage.getItem('books'));
+        }
+        return books;
+    }
+
+    static displayBooks() {
+        const books = Store.getBooks();
+
+        books.forEach(function(book) {
+            const ui = new UI;
+
+            ui.addBookToList(book);
+        });
+    }
+
+    static addBook(book) {
+        const books = Store.getBooks();
+
+        books.push(book);
+
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+
+    static removeBook(isbn) {
+        const books = Store.getBooks();
+
+        books.forEach(function(book, index) {
+            if (book.isbn === isbn) {
+                books.splice(index, 1);
+            }
+        });
+
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+}
+
+// DOM load Event 
+document.addEventListener('DOMContentLoaded', Store.displayBooks);
+
+
+document.getElementById('book-form').addEventListener('submit', function(e) {
+    const title = document.getElementById('title').value,
+        author = document.getElementById('author').value,
+        isbn = document.getElementById('isbn').value
+
+    // instantiate book
+    const book = new Book(title, author, isbn);
+
+    //instentiate book
+    const ui = new UI();
+
+    //validation 
+    if (title === '' || author === '' || isbn === '') {
+        ui.showAlert('Please fill in all fields', 'error');
+    } else {
+        ui.addBookToList(book);
+
+        // add to store
+        Store.addBook(book);
+
+
+        //show success
+        ui.showAlert('Book added!', 'success');
+        //clear fields
+        ui.clearFields();
+    }
+    ui.deleteBook(e.target);
+
+    e.preventDefault();
+});
+// Event Listener for delete
+document.getElementById('book-list').addEventListener('click', function(e) {
+
+    // Instantiate UI
+    const ui = new UI();
+
+    // Delete book
+    ui.deleteBook(e.target);
+
+    //Remove from LS
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+
+    // Show message
+    ui.showAlert('Book Removed!', 'success');
+
+    e.preventDefault();
+});
